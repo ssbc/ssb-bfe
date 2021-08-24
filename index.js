7b1eb510f5c858aeca929d7021ea0d020ff78960
Andre Staltz <andre@staltz.com>
2021-08-24 14:04:28 +0300

    tiny refactor

--- index.js
+++ index.js
@@ -99,9 +99,7 @@ function encode(input) {
 
   // strings
   else if (typeof input === 'string') {
-    if (isSSBURI(input)) {
-      return encoder.ssbURI(input)
-    }
+    if (isSSBURI(input)) return encoder.ssbURI(input)
 
     /* looks for classic sigil/suffix matches */
     const { type, format } = findTypeFormatForSigilSuffix(input, TYPES)
